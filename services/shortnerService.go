6fc9feddb96dbad1a78925bd9ff3c05e0dfe2806
mfvitale <mvitale86@gmail.com>
2022-10-30 11:25:37 +0100

    Chanfe deploy action

--- services/shortnerService.go
+++ services/shortnerService.go
@@ -1,41 +1,41 @@
 package services
 
 import (
-        "os"
-	log "github.com/sirupsen/logrus"
-	"github.com/go-redis/redis/v8"
-	"github.com/mfvitale/curto/repository"
-	"github.com/mfvitale/curto/services/core"
+    "os"
+    log "github.com/sirupsen/logrus"
+    "github.com/go-redis/redis/v8"
+    "github.com/mfvitale/curto/repository"
+    "github.com/mfvitale/curto/services/core"
 )
 
 type shortnerService struct {
-	redisClient *redis.Client
+    redisClient *redis.Client
 }
 
 func NewShortnerService(redisClient *redis.Client) shortnerService {
-	return shortnerService{redisClient}
+    return shortnerService{redisClient}
 }
 
 func (s *shortnerService) Encode(url string) string {
 
-	snowflakeGenerator := core.NewSnowflakeGenerator(int64(os.Getpid()), 10)
+    snowflakeGenerator := core.NewSnowflakeGenerator(int64(os.Getpid()), 10)
 
-	id, err := snowflakeGenerator.NextID()
-	if err != nil {
-		return ""
-	}
+    id, err := snowflakeGenerator.NextID()
+    if err != nil {
+        return ""
+    }
 
-	hashValue, err := core.Base62hash(id)
-	if err != nil {
-		return ""
-	}
+    hashValue, err := core.Base62hash(id)
+    if err != nil {
+        return ""
+    }
 
-	redisRepo := repository.NewRedisUrlRepository(s.redisClient)
+    redisRepo := repository.NewRedisUrlRepository(s.redisClient)
 
-	err = redisRepo.Store(hashValue, url)
-	if err != nil {
-		log.Error(err)
-	}
+    err = redisRepo.Store(hashValue, url)
+    if err != nil {
+        log.Error(err)
+    }
 
-	return hashValue
+    return hashValue
 }
